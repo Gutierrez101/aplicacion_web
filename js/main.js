2545f1b6b019aa830e4a154ad5ed0037695065ea
Gutierrez101 <mateoguti2021info@gmail.com>
2025-05-05 12:26:35 -0500

    descarte de main

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import CartController from './controllers/cartController.js';
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  if(document.getElementById('carritoM')){
-    new CartController();
-  }
-  
-});   
